346a4ba081d3a2dd3d78a1e77c848e35707e5daa
felipesousac <claudioflsousa@gmail.com>
2024-01-07 16:35:04 -0300

    Refactor BookList component to handle case when there are no books in database

--- client/src/components/BookList.jsx
+++ client/src/components/BookList.jsx
@@ -13,19 +13,22 @@ const BookList = () => {
   return (
     <div className="pl-2">
       <h1 className="text-4xl mb-2">Book List</h1>
-      <ul className="list-disc ml-8">
-        {books.map((book) => {
-          const url = `/catalog/book/${book[0].id}`;
-          return (
-            <li key={book[0].id} className="hover:text-neutral-600">
-              <a href={url}>
-                <span className="font-bold">{book[0].title}</span> by {book[2]},{" "}
-                {book[1]}
-              </a>
-            </li>
-          );
-        })}
-      </ul>
+      {books.length ? (
+        <ul className="list-disc ml-8">
+          {books.map((book) => {
+            return (
+              <li key={book[0].id} className="hover:text-neutral-600">
+                <a href={`/catalog/book/${book[0].id}`}>
+                  <span className="font-bold">{book[0].title}</span> by{" "}
+                  {book[2]}, {book[1]}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+      ) : (
+        <div className="text-red-500 font-semibold">No books found</div>
+      )}
     </div>
   );
 };
