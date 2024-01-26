951f514bdcb2afeb27ab44e0742b9639bd47bbfd
felipesousac <claudioflsousa@gmail.com>
2024-01-25 23:05:42 -0300

    Set material ui snackbar when POST request is successful

--- client/src/components/form/GenreForm.jsx
+++ client/src/components/form/GenreForm.jsx
@@ -1,8 +1,18 @@
 import { useState } from "react";
 import axios from "axios";
+import { Snackbar, Alert, Slide } from "@mui/material";
 
 const GenreForm = () => {
   const [name, setName] = useState("");
+  const [open, setOpen] = useState(false);
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
 
   const createGenre = async (e) => {
     e.preventDefault();
@@ -16,7 +26,7 @@ const GenreForm = () => {
     await axios
       .post("http://localhost:8080/catalog/genres", { name }, headers)
       .then((response) => {
-        console.log(response);
+        setOpen(true);
         setName("");
       })
       .catch((error) => {
@@ -24,27 +34,46 @@ const GenreForm = () => {
       });
   };
 
+  // LOGIC TO REMOVE ITEM FROM LIST - USEFULL
+  /*const removeToast = (id) => {
+    setToasts((prevToasts) => prevToasts.filter((toast) => toast.id !== id));
+  };*/
+
   return (
-    <div className="pl-2">
-      <h1 className="text-4xl mb-4">Create Genre</h1>
-      <form className="flex flex-col gap-3" onSubmit={createGenre}>
-        <input
-          className="border border-gray-400 rounded p-2"
-          type="text"
-          value={name}
-          name="name"
-          placeholder="Enter genre name"
-          required
-          onChange={(e) => setName(e.target.value)}
-        />
-        <button
-          type="submit"
-          className="bg-blue-500 transition-colors hover:bg-blue-600 text-white py-2 px-4 rounded"
-        >
-          Create
-        </button>
-      </form>
-    </div>
+    <>
+      <div className="pl-2">
+        <h1 className="text-4xl mb-4">Create Genre</h1>
+        <form className="flex flex-col gap-3 " onSubmit={createGenre}>
+          <input
+            className="border-2 border-gray-400 rounded p-2 focus:border-2 focus:border-blue-600"
+            type="text"
+            value={name}
+            name="name"
+            placeholder="Enter genre name"
+            required
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="bg-blue-500 transition-colors hover:bg-blue-600 text-white py-2 px-4 rounded"
+          >
+            Create
+          </button>
+        </form>
+      </div>
+      {open && (
+        <Snackbar open={open} autoHideDuration={2000} onClose={handleClose}>
+          <Alert
+            onClose={handleClose}
+            severity="success"
+            variant="filled"
+            sx={{ width: "100%" }}
+          >
+            Genre created successfully!
+          </Alert>
+        </Snackbar>
+      )}
+    </>
   );
 };
 
