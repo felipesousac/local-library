393a20f141baccc941dcfa18255d9a6fddcf3569
felipesousac <claudioflsousa@gmail.com>
2024-02-14 11:26:01 -0300

    Creates button to delete author

--- client/src/components/display/AuthorDetailPage.jsx
+++ client/src/components/display/AuthorDetailPage.jsx
@@ -1,11 +1,25 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 
 const AuthorDetailPage = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [author, setAuthor] = useState([]);
+  const [isShowingDelete, setIsShowingDelete] = useState(false);
+
+  const navigate = useNavigate();
   const { id } = useParams();
+
+  async function deleteAuthor() {
+    await axios
+      .delete(`http://localhost:8080/catalog/authors/${id}/delete`)
+      .then((response) => {
+        navigate(`/catalog/authors`);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 
   useEffect(() => {
     axios
@@ -24,7 +38,7 @@ const AuthorDetailPage = () => {
         <>
           <div className="pl-2">
             <h1 className="text-4xl mb-2">
-              Title: {author.lastName}, {author.firstName}
+              {author.lastName}, {author.firstName}
             </h1>
             <p>
               {`Birth: ${author.birthDate}`}
@@ -52,6 +66,30 @@ const AuthorDetailPage = () => {
             ) : (
               <div className="text-red-500 font-semibold ml-5 mt-2">
                 This author has no books in this library.
+              </div>
+            )}
+            <div className="ml-5 mt-4 h-px bg-slate-200" />
+            {!isShowingDelete ? (
+              <button
+                onClick={() => setIsShowingDelete(true)}
+                className="ml-5 mt-4 bg-blue-500 transition-colors hover:bg-blue-600 text-white py-2 px-4 rounded"
+              >
+                Delete author
+              </button>
+            ) : (
+              <div>
+                <button
+                  onClick={() => setIsShowingDelete(false)}
+                  className="ml-5 mt-4 bg-none transition-colors hover:text-blue-800 hover:bg-blue-50 text-blue-600 py-2 px-4 rounded border border-blue-600"
+                >
+                  Cancel
+                </button>
+                <button
+                  onClick={deleteAuthor}
+                  className="ml-5 mt-4 bg-blue-500 transition-colors hover:bg-blue-600 text-white py-2 px-4 rounded"
+                >
+                  Confirm delete
+                </button>
               </div>
             )}
           </div>
